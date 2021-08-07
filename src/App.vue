aef7bf1020a1e6ef13554f4e8b4af3f5c94cf0f6
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-07 13:19:14 +0530

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
-  <router-view />
+  <Header />
+  <div class="container mx-auto">
+    <router-view />
+  </div>
 </template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
