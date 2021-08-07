79ae0603423381c5a49edad381cc80a37b86381b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-07 13:33:59 +0530

    states

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,5 +6,26 @@
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const request = await fetch("https://api.covid19api.com/summary");
+      const data = await request.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
+  },
 };
 </script>
