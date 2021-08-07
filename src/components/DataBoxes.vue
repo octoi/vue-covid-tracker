40b9ed34167dd06911de168c375f3327466e95a6
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-07 13:59:16 +0530

    added comma method

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -4,22 +4,22 @@
       <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
       <div class="text-2xl mb-4">
         <span class="font-bold">New:</span>
-        {{ stats.NewConfirmed }}
+        {{ numberWithCommas(stats.NewConfirmed) }}
       </div>
       <div class="text-2xl mb-4">
         <span class="font-bold">Total:</span>
-        {{ stats.TotalConfirmed }}
+        {{ numberWithCommas(stats.TotalConfirmed) }}
       </div>
     </div>
     <div class="shadow-md bg-blue-100 p-10 text-center rounded">
       <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
       <div class="text-2xl mb-4">
         <span class="font-bold">New:</span>
-        {{ stats.NewDeaths }}
+        {{ numberWithCommas(stats.NewDeaths) }}
       </div>
       <div class="text-2xl mb-4">
         <span class="font-bold">Total:</span>
-        {{ stats.TotalDeaths }}
+        {{ numberWithCommas(stats.TotalDeaths) }}
       </div>
     </div>
   </div>
@@ -29,5 +29,10 @@
 export default {
   name: "DataBoxes",
   props: ["stats"],
+  methods: {
+    numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
+  },
 };
 </script>
