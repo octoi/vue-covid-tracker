6a1272de153f85e402ac11c03bf4e13faf42cc2a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-07 14:05:59 +0530

    displayed all countries

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,8 +1,27 @@
-<template>Select</template>
+<template>
+  <select
+    v-model="selected"
+    class="form-select mt-10 block w-full border p-3 rounded"
+  >
+    <option value="0">Select Country</option>
+    <option
+      v-for="country in countries"
+      v-bind:key="country.ID"
+      :value="country.ID"
+    >
+      {{ country.Country }}
+    </option>
+  </select>
+</template>
 
 <script>
 export default {
   name: "CountrySelect",
   props: ["countries"],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
 };
 </script>
